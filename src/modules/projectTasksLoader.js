e3a0acb21261e12ddefb2840b2726b828c4c597d
abnsol <abenisolo51196@gmail.com>
2024-08-31 11:31:07 +0300

    load Tasks Dynamically

---
+++ src/modules/projectTasksLoader.js
@@ -0,0 +1,166 @@
+import {} from "./project.js";
+import "../styles/singleProject.css";
+
+
+export const singleProject = document.createElement("div");
+singleProject.setAttribute("id","project");
+
+export const displaySingleProject = (project) => {
+    removeProject();
+
+    const projectInfo = document.createElement("div");
+    projectInfo.setAttribute("id","projectInfo");
+
+    const todo = document.createElement("div");
+    todo.setAttribute("id","todo");
+
+    const aboutProject = document.createElement("div");
+    aboutProject.setAttribute("class","aboutProject");
+
+    const noteContainer = document.createElement("div");
+    noteContainer.setAttribute("class","noteContainer");
+
+    const noteTitle = document.createElement("div");
+    noteTitle.setAttribute("class","noteTitle");
+
+    const projectNote = document.createElement("div");
+    projectNote.setAttribute("class","projectNote");
+
+    const descriptionContainer = document.createElement("div");
+    descriptionContainer.setAttribute("class","descriptionContainer");
+
+    const descriptionTitle = document.createElement("div");
+    descriptionTitle.setAttribute("class","descriptionTitle");
+
+    const projectDescription = document.createElement("div");
+    projectDescription.setAttribute("class","projectDescription");
+
+    displayTasks(project.state.tasks,todo);
+    displayProjectInfo(project,projectInfo);
+
+    noteTitle.textContent = "Notes";
+    projectNote.textContent = project.state.notes;
+    descriptionTitle.textContent = "Description";
+    projectDescription.textContent = project.state.description;
+
+
+    noteContainer.appendChild(noteTitle);
+    noteContainer.appendChild(projectNote);
+    descriptionContainer.appendChild(descriptionTitle);
+    descriptionContainer.appendChild(projectDescription);
+    aboutProject.appendChild(noteContainer);
+    aboutProject.appendChild(descriptionContainer);
+    todo.appendChild(aboutProject);
+    singleProject.appendChild(projectInfo);
+    singleProject.appendChild(todo);
+}
+
+const displayTasks = (tasks,todo) => {
+    const projectTasks = document.createElement("div");
+    projectTasks.setAttribute("class","projectTasks");
+
+    tasks.forEach((task) => {
+        console.log(task)
+        const Tasks = document.createElement("div");
+        Tasks.setAttribute("class","Tasks");
+        
+        const taskTitle = document.createElement("div");
+        taskTitle.setAttribute("class","taskTitle");
+        taskTitle.textContent = task.state.title;
+
+        const dueDate = document.createElement("div");
+        dueDate.setAttribute("class","dueDate");
+        dueDate.textContent = task.state.dueDate;
+
+        const urgency = document.createElement("div");
+        urgency.setAttribute("class","urgency");
+        urgency.textContent = task.state.urgent;
+
+        const taskContentWrapper = document.createElement("div");
+        taskContentWrapper.setAttribute("class","taskContentWrapper");
+
+        const taskContent = document.createElement("div");
+        taskContent.setAttribute("class","taskContent");
+
+        const description = document.createElement("div");
+        description.setAttribute("class","description");
+        description.textContent = task.state.description;
+        
+        const note = document.createElement("div");
+        note.setAttribute("class","note");
+        note.textContent = task.state.notes;
+
+        Tasks.appendChild(taskTitle);
+        Tasks.appendChild(dueDate);
+        Tasks.appendChild(urgency);
+        taskContent.appendChild(description);
+        taskContent.appendChild(note);
+        taskContentWrapper.appendChild(taskContent)
+        Tasks.appendChild(taskContentWrapper);
+
+        console.log(projectTasks);
+        projectTasks.appendChild(Tasks);
+    })
+    todo.appendChild(projectTasks);
+}
+
+const displayProjectInfo = (project,projectInfo) => {
+    const projectInfoTitle = document.createElement("div");
+    projectInfoTitle.setAttribute("class","projectInfoTitle");
+
+    const projectInfoAdditional = document.createElement("div");
+    projectInfoAdditional.setAttribute("class","projectInfoAdditional");
+
+    const left = document.createElement("div");
+    left.setAttribute("class","left");
+
+    const urgencyContainer = document.createElement("div");
+    urgencyContainer.setAttribute("class","urgencyContainer");
+    const dueDateContainer = document.createElement("div");
+    dueDateContainer.setAttribute("class","dueDateContainer");
+    const numberOfTasksContainer = document.createElement("div");
+    numberOfTasksContainer.setAttribute("class","numberOfTasksContainer");
+
+    projectInfoTitle.textContent = project.state.title;
+    urgencyContainer.textContent = project.state.urgent;
+    dueDateContainer.textContent = project.state.dueDate;
+
+    const right = document.createElement("div");
+    right.setAttribute("class","right");
+
+    const addTaskButton = document.createElement("button");
+    addTaskButton.setAttribute("class","addTaskButton");
+    const deleteProjectButton = document.createElement("button");
+    deleteProjectButton.setAttribute("class","deleteProjectButton");
+    const editProjectButton = document.createElement("button");
+    editProjectButton.setAttribute("class","editProjectButton");
+
+    addTaskButton.textContent = "Add Task";
+    deleteProjectButton.textContent = "Delete Project";
+    editProjectButton.textContent = "Edit Project";
+
+
+
+
+    left.appendChild(urgencyContainer);
+    left.appendChild(dueDateContainer);
+    left.appendChild(numberOfTasksContainer);
+    right.appendChild(addTaskButton);
+    right.appendChild(editProjectButton);
+    right.appendChild(deleteProjectButton);
+    projectInfoAdditional.appendChild(left);
+    projectInfoAdditional.appendChild(right);
+    projectInfo.appendChild(projectInfoTitle);
+    projectInfo.appendChild(projectInfoAdditional);
+}
+
+export const expandTask = (event) => {
+    const expand = event.target.closest(".Tasks");
+    expand.classList.toggle("Tasks-Open");
+}
+
+export const removeProject = () => {
+    while (singleProject.firstChild){
+        singleProject.removeChild(singleProject.firstChild);
+    }
+}
